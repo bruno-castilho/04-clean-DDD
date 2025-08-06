21772d9c6fa96ca1d299547c8574b233b8aa5d5e
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-08-06 12:57:45 -0300

    Refatorando testes unitários

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,6 +1,6 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
-import { Either, right } from '@/core/either'
+import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface GetQuestionBySlugUseCaseRequest {
@@ -23,7 +23,7 @@ export class GetQuestionBySlugUseCase {
     const question = await this.questionsRepository.findBySlug(slug)
 
     if (!question) {
-      throw new Error('Question not found.')
+      return left(new ResourceNotFoundError())
     }
 
     return right({
